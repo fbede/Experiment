e74f647890d6ade0296b6f59203bd39d9c4028bd
favour bede <favourbede@gmail.com>
2020-03-25 16:53:02 +0100

    First contraint layout commit

---
+++ app/src/main/java/com/example/learningcontraintlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.learningcontraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
